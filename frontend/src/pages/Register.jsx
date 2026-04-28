4f2f464732a65b4236fe6491825c99ec63d65233
HafsaSarfaraz <154260956+HafsaSarfaraz@users.noreply.github.com>
2026-04-29 00:43:38 +0500

    Improve login ui

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Image from "../assets/image.png";
 import Logo from "../assets/logo.png";
-import GoogleSvg from "../assets/icons8-google.svg";
 import { FaEye } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 import "../styles/Register.css";
@@ -147,15 +146,11 @@ const Register = () => {
 
               <div className="register-center-buttons">
                 <button type="submit">Sign Up</button>
-                <button type="submit">
-                  <img src={GoogleSvg} alt="" />
-                  Sign Up with Google
-                </button>
               </div>
             </form>
           </div>
 
-          <p className="login-bottom-p">
+          <p className="register-bottom-p">
             Already have an account? <Link to="/login">Login</Link>
           </p>
         </div>
